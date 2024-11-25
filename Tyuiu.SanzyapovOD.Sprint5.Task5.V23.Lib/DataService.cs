ae42db1952112e92c204c66dcd26a78e8722a724
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-11-25 07:33:34 +0500

    Task5

---
+++ Tyuiu.SanzyapovOD.Sprint5.Task5.V23.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.SanzyapovOD.Sprint5.Task5.V23.Lib
+{
+    public class DataService : ISprint5Task5V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double min = 100000;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Convert.ToDouble(line) < min)
+                    {
+                        min = Convert.ToDouble(line);
+                    }
+                }
+            }
+            return Math.Round(min, 3);
+        }
+    }
+}
---
+++ Tyuiu.SanzyapovOD.Sprint5.Task5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SanzyapovOD.Sprint5.Task5.V23.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint5.Task5.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V23.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
